b0f185e5fef542cac91e25a9d2098df92ec0d2db
Kevin Conner <kconner@redhat.com>
2019-12-14 08:18:47 -0800

    Tidy up init

--- servicemesh/1-introduction/set-env.sh
+++ servicemesh/1-introduction/set-env.sh
@@ -1,4 +1,3 @@
-echo "Downloading Istio..."
-until $(tar -tvf /root/installation/istio-1.4.2-linux.tar.gz &> /dev/null); do (echo -n .; sleep 2); done;
+echo "Waiting for Initialisation to complete"
+until [ -f /tmp/initialised ] ; do (echo -n .; sleep 2); done;
 /usr/local/bin/launch.sh
-mkdir -p /root/installation && cd ~/installation
